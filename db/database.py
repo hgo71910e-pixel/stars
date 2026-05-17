23d3f2da4731e5b35f39b7f16bebed3a6b73512f
os888812-glitch <os888812@gmail.com>
2026-05-17 20:33:41 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -10,7 +10,7 @@ _pool = None
 async def get_pool():
     global _pool
     if _pool is None:
-        _pool = await asyncpg.create_pool(DATABASE_URL)
+        _pool = await asyncpg.create_pool(DATABASE_URL, ssl="require")
     return _pool
 
 
